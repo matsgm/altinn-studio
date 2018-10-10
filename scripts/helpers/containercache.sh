6ca84ca26cc0739610af77c7ead2ac666cba846f
Mats Gimming Myhre <mats.g.myhre@gmail.com>
2018-10-10 10:37:01 +0200

    Added Docker Login to altinntjenestercontainerregistry.

--- scripts/helpers/containercache.sh
+++ scripts/helpers/containercache.sh
@@ -27,6 +27,9 @@ systemctl status docker --no-pager
 docker pull microsoft/dotnet@sha256:c50b596c93b11167ef7c6174a2717a18382d28965c11984691a0479c990ada3d
 #dotnet:2.1-aspnetcore-runtime
 docker pull microsoft/dotnet@sha256:b99ecca89d5a1d8919782f01b639d2376267456f6b7b5cc1b9d7fca94f579869
+
+#login to altinntjenestercontainerregistry
+docker login $ACR_URL -u $ACR_NAME -p $ACR_PASSWORD
 #altinn-runtime
 docker pull altinntjenestercontainerregistry.azurecr.io/altinn-runtime:83
 
