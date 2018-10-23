5a491c45970e2e3f5774b25d4f06e80608c6c673
Mats Gimming Myhre <mats.myhre@capgemini.com>
2018-10-23 16:51:02 +0200

    Update CalculationHandler.cs

--- editions/2004/Implementation/CalculationHandler.cs
+++ editions/2004/Implementation/CalculationHandler.cs
@@ -7,6 +7,7 @@ namespace AltinnCoreServiceImplementation.ninaOrg.repo1_4
     {
         public CalculationHandler()
         {
+            // Some commented out code
         }
 
         public void Calculate(Skjema Skjema)
